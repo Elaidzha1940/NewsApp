8117871bcdae656eca2aeffd217c598e5e32342c
Elaidzha1940 <eli080293@gmail.com>
2023-08-27 13:42:37 +0300

    NewsApp

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Elaidzha Shchukin on 27.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
