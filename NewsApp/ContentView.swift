ead4d398b52847c15dacd9516c8b6e071f88768c
Elaidzha1940 <eli080293@gmail.com>
2023-08-27 13:46:15 +0300

    NewsApp + added MVVM

--- NewsApp/ContentView.swift
+++ NewsApp/ContentView.swift
@@ -1,21 +1,22 @@
-//  
-//  ContentView.swift
-//  NewsApp
+//  /*
 //
-//  Created by Elaidzha Shchukin on 27.08.2023.
+//  Project: NewsApp
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 27.08.2023
 //
-
+//  Status: in progress | Decorated
+//
+//  */
+ 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+           
         }
-        .padding()
     }
 }
 
--- NewsApp/NewsAppApp.swift
+++ NewsApp/NewsAppApp.swift
@@ -1,9 +1,13 @@
+//  /*
 //
-//  NewsAppApp.swift
-//  NewsApp
+//  Project: NewsApp
+//  File: NewsAppApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 27.08.2023
 //
-//  Created by Elaidzha Shchukin on 27.08.2023.
+//  Status: in progress | Decorated
 //
+//  */
 
 import SwiftUI
 
