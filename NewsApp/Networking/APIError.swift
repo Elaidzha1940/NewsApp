157df369ddd0d610cd33346ad8800036587b5125
Elaidzha1940 <eli080293@gmail.com>
2023-08-27 19:24:16 +0300

    APIError

--- NewsApp/Networking/APIError.swift
+++ NewsApp/Networking/APIError.swift
@@ -1,8 +1,33 @@
+//  /*
 //
-//  APIError.swift
-//  NewsApp
+//  Project: NewsApp
+//  File: APIError.swift
+//  Created by Elaidzha Shchukin
+//  Date: 27.08.2023
 //
-//  Created by Elaidzha Shchukin on 27.08.2023.
+//  Status: in progress | Decorated
 //
+//  */
 
 import Foundation
+
+enum APIError: Error {
+    
+    case decodingError
+    case errorCode(Int)
+    case unknown
+}
+
+extension APIError: LocalizedError {
+    var errorDescription: String? {
+        
+        switch self {
+        case .decodingError:
+            return "Failed to decode the object from the service"
+        case .errorCode(let code):
+            return "\(code) - Something went wrong"
+        case .unknown:
+            return "The error is unknown"
+        }
+    }
+}
