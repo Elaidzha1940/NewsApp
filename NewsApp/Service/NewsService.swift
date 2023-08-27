52cb5f4b60c52e412b0cb8215e334cce567cf778
Elaidzha1940 <eli080293@gmail.com>
2023-08-27 19:42:55 +0300

    NewsService

--- NewsApp/Service/NewsService.swift
+++ NewsApp/Service/NewsService.swift
@@ -16,3 +16,28 @@ protocol NewsService {
     
     func request(from endpoint: NewsAPI) -> AnyPublisher<News, APIError>
 }
+
+struct NewsServiceImpl: NewsService {
+    
+    func request(from endpoint: NewsAPI) -> AnyPublisher<News, APIError> {
+        
+        return URLSession
+            .shared
+            .dataTaskPublisher(for: endpoint.urlRequest)
+            .receive(on: DispatchQueue.main)
+            .mapError { _ in APIError.unknown }
+            .flatMap { data, response -> AnyPublisher<News, APIError> in
+                
+                guard let response = response as? HTTPURLResponse else {
+                    return Fail(error: APIError.unknown).eraseToAnyPublisher()
+                }
+                
+                if (200...299).contains(response.statusCode) {
+                    
+                } else {
+                    
+                }
+            }
+            .eraseToAnyPublisher()
+    }
+}
