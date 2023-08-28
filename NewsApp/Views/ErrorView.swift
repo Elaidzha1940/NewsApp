396c018b8c567f403ff557e46c0d98f732ade75e
Elaidzha1940 <eli080293@gmail.com>
2023-08-28 16:34:41 +0300

    ErrorView

--- NewsApp/Views/ErrorView.swift
+++ NewsApp/Views/ErrorView.swift
@@ -12,14 +12,45 @@
 import SwiftUI
 
 struct ErrorView: View {
+    let error = Error.self
+    
     var body: some View {
         
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image(systemName: "exclamtionmark.icloud.fill")
+                .foregroundColor(.red)
+                .font(.system(size: 40, weight: .bold, design: .rounded))
+                .padding(.bottom, 5)
+            
+            Text("Ooops")
+                .foregroundColor(.red)
+                .font(.system(size: 30, weight: .bold, design: .rounded))
+                .multilineTextAlignment(.center)
+            
+            Text(error.localizedDescription)
+                .foregroundColor(.red)
+                .font(.system(size: 20, weight: .bold, design: .rounded))
+                .multilineTextAlignment(.center)
+                .padding(.vertical, 5)
+            
+            Button {
+                //action
+            } label: {
+                Text("Retry")
+            }
+            .padding(.vertical, 12)
+            .padding(.horizontal, 30)
+            .background(Color.red)
+            .foregroundColor(.white)
+            .font(.system(size: 20, weight: .bold, design: .rounded))
+            .cornerRadius(20)
+
+        }
     }
 }
 
 struct ErrorView_Previews: PreviewProvider {
     static var previews: some View {
-        ErrorView()
+        ErrorView(error: APIError.decodingError)
     }
 }
