e27a94fef0662a88cf376baf372331f195b0c32c
Elaidzha1940 <eli080293@gmail.com>
2023-08-28 12:40:40 +0300

    added NewsViewModel

--- NewsApp/VeiwModel/NewsViewModel.swift
+++ NewsApp/VeiwModel/NewsViewModel.swift
@@ -15,3 +15,39 @@ import Combine
 protocol NewsViewModel {
     func getArticles()
 }
+
+
+class NewsViewModelImpl: ObservableObject, NewsViewModel {
+    
+    private let service: NewsService
+    
+    private(set) var articles = [Article]()
+    private var cancellables = Set<AnyCancellable>()
+    
+    @Published private(set) var state: ResultState = .loading
+    
+    init(service: NewsService) {
+        self.service = service
+        
+    }
+    
+    func getArticles() {
+        
+        let cancellables = service
+            .request(from: .getNews)
+            .sink { res in
+                switch res {
+                    
+                case .finished:
+                    // Send back the articles
+                    break
+                case .failure(let error):
+                    // Send back the error
+                    break
+                }
+            } receiveValue: { response in
+                self.articles = response.articles
+            }
+        
+    }
+}
