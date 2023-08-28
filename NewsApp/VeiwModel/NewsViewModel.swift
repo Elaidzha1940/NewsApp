11dd844d0ee6d288be4d3a2f4f6b5c5137a14d7a
Elaidzha1940 <eli080293@gmail.com>
2023-08-28 12:28:10 +0300

    added NewsViewModel

---
+++ NewsApp/VeiwModel/NewsViewModel.swift
@@ -0,0 +1,17 @@
+//  /*
+//
+//  Project: NewsApp
+//  File: NewsViewModel.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 27.08.2023
+//
+//  Status: in progress | Decorated
+//
+//  */
+
+import Foundation
+import Combine
+
+protocol NewsViewModel {
+    func getArticles()
+}
